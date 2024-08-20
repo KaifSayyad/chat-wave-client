a2b17fed87e3c00571aba24ad08402232cb81284
Kaif Sayyad <114498073+KaifSayyad@users.noreply.github.com>
2024-08-21 00:56:19 +0530

    Update firebase.js

--- client/firebase.js
+++ client/firebase.js
@@ -9,13 +9,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCyoP4G25ENZ3VJRP1ZixxEeEQhYhSRcWI",
-  authDomain: "chat-app-4a5eb.firebaseapp.com",
-  projectId: "chat-app-4a5eb",
-  storageBucket: "chat-app-4a5eb.appspot.com",
-  messagingSenderId: "299047134705",
-  appId: "1:299047134705:web:fcf623734c829c6fa69050",
-  measurementId: "G-ZPGRTQ70M2"
+  apiKey: YOUR API KEY,
+  authDomain: .....
+  projectId: ....
+  storageBucket:.....
+  messagingSenderId: .....
+  appId: .....
+  measurementId: ...
 };
 
 // Initialize Firebase
