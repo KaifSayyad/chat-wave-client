24b785a0b0667225e6d35669688397dcd37008b2
KaifSayyad <kaifalisayyad@gmail.com>
2024-08-27 16:42:31 +0530

    Delete dist-1.1.0

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['react-loader-spinner'], // Add the module you want to externalize here
+    },
+  },
   optimizeDeps: {
     exclude: ['js-big-decimal']
   },
