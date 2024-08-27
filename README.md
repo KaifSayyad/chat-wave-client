27f72f79593dce2252b9e850bd8924d42a0aa4b2
KaifSayyad <kaifalisayyad@gmail.com>
2024-08-27 16:40:46 +0530

    Delete dist-1.1.0

--- README.md
+++ README.md
@@ -46,7 +46,7 @@ Ensure you have the following installed on your machine:
     ```
 
     Also you'll need to set up Firebase auth. 
-    If you want to use mine mail me at [mail](kaifalisayyad@gmail.com) with SUBJECT : ENV KEYS FOR CHATWAVE
+    If you want to use mine [mail](kaifalisayyad@gmail.com) me with SUBJECT : ENV KEYS FOR CHATWAVE
 
 NOTE : Here Port Number (default is 9999) should be same as "NGINX_PORT" in ./chat-wave-server/.env file
 
--- release-build.sh
+++ release-build.sh
@@ -3,9 +3,8 @@
 # Prompt the user for the version number
 read -p "Enter the version number: " version
 
-
 # Build the client
-cd client
+cd client || exit
 npm install
 npm run build
 cd ..
@@ -19,17 +18,18 @@ cp -r client/dist/* "$folder_name"
 
 # Push the new folder to the target repository
 git clone https://github.com/KaifSayyad/chat-wave-client-build-files.git
-cd chat-wave-client-build-files
+cd chat-wave-client-build-files || exit
 cp -r ../"$folder_name" .
 git add "$folder_name"
 git commit -m "Add $folder_name"
 git push origin main
 
-# Delete the folder from the current repository
+# Cleanup: Delete the folder from the current repository
 cd ..
 rm -rf "$folder_name"
 git add .
 git commit -m "Delete $folder_name"
 git push origin main
 
+# Remove the cloned repository
 rm -rf chat-wave-client-build-files
